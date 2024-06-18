e2076044eda2df0a968d8cac7006af67bf92b286
Sebastian Gonzalez <sebas94gonzalez@gmail.com>
2024-06-18 17:53:56 -0500

    Working on 'Press & Hold'

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import setup_and_login as snl
 import simulate_input as input
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
 
 
 driver, actions, wait = snl.driver_setup()
-
-url = 'https://www.fiverr.com/login'
+time.sleep(1)
+url = 'https://www.fiverr.com'
 driver.get(url)
+print(driver.find_element(By.CSS_SELECTOR,'#px-captcha'))
+element = driver.find_element(By.CSS_SELECTOR,'#px-captcha')
+actions.click_and_hold(element)
+actions.perform()
 time.sleep(10)
+actions.release(element)
+actions.perform()
+time.sleep(0.2)
+actions.release(element)
 # input.element_click(driver, actions, wait, '//*[@id="__ZONE__main"]/div/div/div/div/div/div/section/div/div[2]/section/section[1]/button[1]/p')
-time.sleep(13)
+# time.sleep(13)
 # Locate the "Press & Hold" button
-##button = driver.find_element_by_xpath('//button[contains(text(), "Press ")]')
+##button = driver.find_element(By.XPATH,'//button[contains(text(), "Press ")]')
 
 # Perform a long press (click and hold)
 ##actions.click_and_hold(button).perform()
--- setup_and_login.py
+++ setup_and_login.py
@@ -9,6 +9,7 @@ import time
 from dotenv import load_dotenv
 import os
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
 
 # Load environment variables from .env file
 load_dotenv()
@@ -17,16 +18,27 @@ def driver_setup():
 
     # Set up Chrome options to use the existing profile
     chrome_options = Options()
-    chrome_profile = r'C:\Users\se_ba\AppData\Local\Google\Chrome\User Data'  # Change this path to your profile path
+    # chrome_profile = r'C:\Users\se_ba\AppData\Local\Google\Chrome\User Data'  # Change this path to your profile path
+    chrome_profile = r'C:\Users\se_ba\AppData\Local\Google\Chrome' # Guest profile
     chrome_options.add_argument(f"user-data-dir={chrome_profile}")
-    chrome_options.add_argument("profile-directory=Default")  # Name of the profile directory
+    # chrome_options.add_argument("profile-directory=Profile 10")  # Name of the profile directory
+    # chrome_options.add_argument("profile-directory=Profile 1")  # Name of the profile directory
+
+    # Add additional options to ensure Chrome starts correctly
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.add_argument("--disable-dev-shm-usage")
+    # chrome_options.add_argument("--remote-debugging-port=9222")
+    # chrome_options.add_argument("--headless")  # Run Chrome in headless mode for testing, remove if you need a visible browser
+    # chrome_options.add_argument("--disable-gpu")  # Applicable for Windows OS
 
     # Specify the path to the Chrome executable if it is not in the default location
-    chrome_options.binary_location = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+    chrome_options.binary_location = r'C:\Program Files\Google\Chrome\Application\chrome.exe'
+    # chrome_options.binary_location = r'C:\Users\se_ba\.cache\selenium\chromedriver\win64\126.0.6478.61\chromedriver.exe'
 
     # chrome_options = webdriver.ChromeOptions()
     chrome_options.add_experimental_option("detach", True)  # TODO: Modify as needed for debug mode
-    driver = webdriver.Chrome(options=chrome_options)    
+    driver = webdriver.Chrome(options=chrome_options)
+    
 
     # options = webdriver.ChromeOptions()
     # options.add_experimental_option("detach", True)  # TODO: Modify as needed for debug mode
