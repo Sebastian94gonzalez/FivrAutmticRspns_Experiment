cb4853a3e3da7e3bc234bb6170c50fe5152b6a32
Sebastian Gonzalez <sebas94gonzalez@gmail.com>
2024-06-21 07:26:13 -0500

    Failed attempts on 'Press & Hold'

--- main.py
+++ main.py
@@ -3,16 +3,51 @@ import simulate_input as input
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
+import re
 
+
+def captchaCheck():
+    
+    pass
 
 driver, actions, wait = snl.driver_setup()
 time.sleep(1)
 url = 'https://www.fiverr.com'
 driver.get(url)
 print(driver.find_element(By.CSS_SELECTOR,'#px-captcha'))
-element = driver.find_element(By.CSS_SELECTOR,'#px-captcha')
+# element = driver.find_element(By.CSS_SELECTOR,'#px-captcha')
+element = driver.find_element(By.XPATH,'/html/body/div/div/div[2]/div[2]/p')
+print(element.get_attribute('innerHTML'))
 actions.click_and_hold(element)
 actions.perform()
+time.sleep(1)
+notFinished = True
+
+articleElement = driver.find_element(By.TAG_NAME, 'article')
+print('articleElement\n' + str(articleElement))
+styleElements = driver.find_elements(By.TAG_NAME, 'style')
+print('styleElements\n' + str(styleElements))
+styles_test = driver.find_elements(By.XPATH, '/html/head/style')
+
+print('styles_test\n' + str(styles_test))
+
+class_pattern = re.compile(r'pointer\}(.*?)\{')
+
+# for style in styleElements:
+#     style_text = style.get_attribute('innerHTML')
+#     print(style_text)
+#     matches = class_pattern.findall(style_text)
+#     for match in matches:
+#         print(f"Extracted text: {match}")
+    # if class_pattern.match(style.get_attribute('body')):
+    #     continue
+
+# while (notFinished):
+#     articleElement = driver.find_element(By.TAG_NAME, '<article>')
+#     styleElements = driver.find_element(By.TAG_NAME, '<style>')
+#     styles_test = driver.find_elements(By.XPATH, '/html/head/style')
+#     pass
+
 time.sleep(10)
 actions.release(element)
 actions.perform()
